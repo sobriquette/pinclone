dad53f488333027c05ffeaa9cb5ea156010578b9
Melody Lin <11lin.m7@gmail.com>
2017-11-05 23:13:51 -0500

    create base template and nav

--- pinterest/views.py
+++ pinterest/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Image, Pin
+from .models import Pin
 
 def index(request):
 	context = {'pins_list': Pin.objects.all()}
