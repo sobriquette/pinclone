02ecdb27ae1881d4d5561c0bc9ec7f21d6a2168b
Melody Lin <11lin.m7@gmail.com>
2017-11-05 23:45:41 -0500

    add webpack and babel to frontend

---
+++ pinterest/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+context: __dirname,
+
+entry: './assets/js/index.js', 
+
+output: {
+	path: path.resolve('./assets/bundles/'),
+	filename: "[name]-[hash].js",
+},
+
+plugins: [
+	new BundleTracker({filename: './webpack-stats.json'}),
+],
+
+module: {
+	loaders: [
+	{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+	{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+
+	],
+},
+
+}
