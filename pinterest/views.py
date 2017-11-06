63c0492089a59f97e2d07d93cf6d203eee78646a
Melody Lin <11lin.m7@gmail.com>
2017-11-05 21:33:55 -0500

    add css styling for pins

--- pinterest/views.py
+++ pinterest/views.py
@@ -4,25 +4,5 @@ from django.template import loader
 from .models import Image, Pin
 
 def index(request):
-	# all_imgs_list = {}
-	
-	# for img in Image.objects.all().select_related('pin'):
-	# 	all_imgs_list[img.image_id] = {}
-	# 	pin = img.pin
-
-	# 	all_imgs_list[img.image_id]['url'] = img.url
-	# 	all_imgs_list[img.image_id]['pin'] = {}
-
-	# 	# TODO: Refactor so we don't have to list these individually
-	# 	all_imgs_list[img.image_id]['pin']['description'] = pin.description
-	# 	all_imgs_list[img.image_id]['pin']['like_count'] = pin.like_count
-	# 	all_imgs_list[img.image_id]['pin']['link'] = pin.link
-	# 	all_imgs_list[img.image_id]['pin']['title'] = pin.title
-
-	pins = Pin.objects.all()
-	template = loader.get_template('pinterest/index.html')
-	context = {
-		'pins_list': pins,
-	}
-
-	return HttpResponse(template.render(context, request))
+	context = {'pins_list': Pin.objects.all()}
+	return render(request, 'pinterest/index.html', context)
